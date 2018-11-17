82fd13d4bcfb20bee2ec9367cc0eb66a462263b2
kasturinaveen <43032059+kasturinaveen@users.noreply.github.com>
2018-11-17 12:32:57 +0530

    bug fixes

--- sigverification.py
+++ sigverification.py
@@ -10,7 +10,7 @@ import tensorflow as tf
 np.random.seed(123)
 from keras.models import Sequential, Model, load_model
 from keras.layers import Dense, Dropout, Activation, Flatten, Lambda
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, MaxPool2D
 from keras.layers import Input, concatenate
 from keras.utils import np_utils
 from keras.layers.normalization import BatchNormalization
@@ -33,11 +33,9 @@ from preprocess_image import preprocess_image
 # Parameters
 
 def contrastive_loss(y_true, y_pred):
-    '''Contrastive loss from Hadsell-et-al.'06
-    http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
-    '''
     margin = 1
-    return K.mean(y_true * K.square(y_pred) + (1 - y_true) * K.square(K.maximum(margin - y_pred, 0)))
+    print(K.mean(y_true * K.square(y_pred) + (1 - y_true) * K.square(K.maximum(margin - y_pred, 0))))
+    return (K.mean((1-y_true) * K.square(y_pred) + (y_true) * K.square(K.maximum(margin - y_pred, 0))))
 
 
 def euclidean_distance(vects):
@@ -50,85 +48,9 @@ def eucl_dist_output_shape(shapes):
     return (shape1[0], 1)
 
 
-global samples_per_train
-samples_per_train = 50
-global cur_train_index
-cur_train_index = 0
-global batch_sz
-batch_sz = 50
-path = "D:\\nkasturi122817\\signatureverification\\NISDCC-offline-all-001-051-6g\\"
-dir = "D:\\nkasturi122817\\signatureverification\\NISDCC-offline-all-001-051-6g"
-
-def next_train():
-    while True:
-        global cur_train_index
-        print(samples_per_train)
-        if cur_train_index == samples_per_train:
-            cur_train_index = 0
-
-        temp_cur_train_index = cur_train_index + batch_sz
-
-        if temp_cur_train_index > samples_per_train:
-            temp_cur_train_index = samples_per_train
-        selected_pairs = pairs.iloc[cur_train_index:temp_cur_train_index, :]
-        #print("cur_train_index " + str(cur_train_index) + "selected_pairs.names" + str(selected_pairs.names))
-        image_pairs = []
-        label_pairs = []
-
-        for index, rows in selected_pairs.iterrows():
-            #print(index)
-            img1 = preprocess_image(glob.glob(path + "NISDCC-" + rows.names + "_6g.PNG")[0])
-            # print(img1)
-
-            img1 = prep_image.img_to_array(img1)  # , dim_ordering='tf')
-
-            img2 = preprocess_image(glob.glob(path + "NISDCC-" + rows.names_2 + "_6g.PNG")[0])
-
-            img2 = prep_image.img_to_array(img2)  # , dim_ordering='tf')
-
-            image_pairs += [[img1, img2]]
-            label_pairs += [int(rows.Label)]
-
-        cur_train_index = temp_cur_train_index
-
-        images = [np.array(image_pairs)[:, 0], np.array(image_pairs)[:, 1]]
-        labels = np.array(label_pairs)
-        yield (images, labels)
 
 
-def next_valid():
-    while True:
-        global cur_train_index
-        if cur_train_index == samples_per_train:
-            cur_train_index = 0
 
-        temp_cur_train_index = cur_train_index + batch_sz
-
-        if temp_cur_train_index > samples_per_train:
-            temp_cur_train_index = samples_per_train
-        selected_pairs = pairs.iloc[cur_train_index:temp_cur_train_index, :]
-        image_pairs = []
-        label_pairs = []
-
-        for index, rows in selected_pairs.iterrows():
-            img1 = preprocess_image(glob.glob(path + "NISDCC-" + rows.names + "_6g.PNG")[0])
-            #print(img1)
-
-            img1 = prep_image.img_to_array(img1)  # , dim_ordering='tf')
-
-            img2 = preprocess_image(glob.glob(path + "NISDCC-" + rows.names_2 + "_6g.PNG")[0])
-
-            img2 = prep_image.img_to_array(img2)  # , dim_ordering='tf')
-
-            image_pairs += [[img1, img2]]
-            label_pairs += [int(rows.Label)]
-
-        cur_train_index = temp_cur_train_index
-
-        images = [np.array(image_pairs)[:, 0], np.array(image_pairs)[:, 1]]
-        labels = np.array(label_pairs)
-#        print(images.shape)
-        yield (images, labels)
 
 
 def create_base_network_signet(input_shape):
@@ -141,51 +63,102 @@ def create_base_network_signet(input_shape):
     model.add(Flatten())
     model.add(Dense(1024, activation="relu"))
     model.add(Dropout(0.5))
-    model.add(Dense(128, W_regularizer=l2(0.0005), activation='relu', init='glorot_uniform'))  # softmax changed to relu
-
+    model.add(Dense(128, activation="sigmoid"))
+    # convolutional_net = Sequential()
+    # convolutional_net.add(Conv2D(filters=64, kernel_size=(10, 10),
+    #                              activation='relu',
+    #                              input_shape=input_shape,
+    #                              name='Conv1'))
+    # convolutional_net.add(MaxPool2D())
+    #
+    # convolutional_net.add(Conv2D(filters=128, kernel_size=(7, 7),
+    #                              activation='relu',
+    #                              name='Conv2'))
+    # convolutional_net.add(MaxPool2D())
+    #
+    # convolutional_net.add(Conv2D(filters=128, kernel_size=(4, 4),
+    #                              activation='relu',
+    #                              name='Conv3'))
+    # convolutional_net.add(MaxPool2D())
+    #
+    # # convolutional_net.add(Conv2D(filters=256, kernel_size=(4, 4),
+    # #                              activation='relu',
+    # #                              name='Conv4'))
+    # # convolutional_net.add(MaxPool2D())
+    #
+    # convolutional_net.add(Flatten())
+    # convolutional_net.add(
+    #     Dense(units=4096, activation='relu',
+    #           name='Dense1'))
+    # convolutional_net.add(
+    #     Dense(units=128, activation='sigmoid',
+    #           name='Dense11'))
     return model
 
 
-#if __name__ == '__main__':
+def compute_accuracy_roc(predictions, labels):
+    '''Compute ROC accuracy with a range of thresholds on distances.
+    '''
+    dmax = np.max(predictions)
+    dmin = np.min(predictions)
+    nsame = np.sum(labels == 1)
+    ndiff = np.sum(labels == 0)
 
-input_shape = (62,128,1)
-params = {'dim': input_shape,
-          'batch_size': 64,
-          'n_classes': 2,
-          'n_channels': 1,
-          'shuffle': True}
-pairs, train_IDs, valid_IDs = pairs_generator('D:\\nkasturi122817\\signatureverification\\NISDCC-offline-all-001-051-6g\\data.csv')
-partition = {'train':train_IDs,'validation': valid_IDs}
-training_generator = DataGenerator(partition['train'], pairs, path, **params)
-validation_generator = DataGenerator(partition['validation'], pairs, path, **params)
-base_network = create_base_network_signet(input_shape)
-input_a = Input(shape=input_shape)
-input_b = Input(shape=input_shape)
-processed_a = base_network(input_a)
-processed_b = base_network(input_b)
-distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([processed_a, processed_b])
-model = Model(input=[input_a, input_b], output=distance)
-rms = RMSprop(lr=1e-4, rho=0.9, epsilon=1e-08)
-adadelta = Adadelta()
-model.compile(loss=contrastive_loss, optimizer=rms)
+    step = 0.01
+    max_acc = 0
 
-model.fit_generator(generator=training_generator,
-                    validation_data=validation_generator,
-                    use_multiprocessing=True,
-                    workers=6)
-# model.save(os.path.join(dir,"model.h5"))
-#model = load_model(os.path.join(dir,"model.h5"))
-# for index, rows in pairs.iterrows():
-#     img1 = preprocess_image(glob.glob(path + "NISDCC-" + rows.names + "_6g.PNG")[0])
-#     # print(img1)
-#
-#     img1 = prep_image.img_to_array(img1)  # , dim_ordering='tf')
-#
-#     img2 = preprocess_image(glob.glob(path + "NISDCC-" + rows.names_2 + "_6g.PNG")[0])
-#
-#     img2 = prep_image.img_to_array(img2)  # , dim_ordering='tf')
-prediction = model.predict_generator(generator=next_train(), steps=2, max_queue_size=10, workers=1, use_multiprocessing=True, verbose=0)
-print(prediction)
-#pairs.iloc[index,8] = model.predict([img1,img2])
-#pairs.iloc[index, 9] = model.predict(img2)
-# pairs.to_csv('test.csv')
+    for d in np.arange(dmin, dmax + step, step):
+        idx1 = predictions.ravel() <= d
+        idx2 = predictions.ravel() > d
+
+        tpr = float(np.sum(labels[idx1] == 1)) / nsame
+        tnr = float(np.sum(labels[idx2] == 0)) / ndiff
+        acc = 0.5 * (tpr + tnr)
+        #       print ('ROC', acc, tpr, tnr)
+
+        if (acc > max_acc):
+            max_acc = acc
+
+    return max_acc
+
+if __name__ == '__main__':
+    path = "D:\\nkasturi122817\\signatureverification\\NISDCC-offline-all-001-051-6g\\"
+    dir = "D:\\nkasturi122817\\signatureverification\\NISDCC-offline-all-001-051-6g"
+
+    input_shape = (62,128,1)
+    params = {'dim': input_shape,
+              'batch_size': 200,
+              'n_classes': 2,
+              'n_channels': 1,
+              'shuffle': True}
+    pairs, train_IDs, valid_IDs = pairs_generator('D:\\nkasturi122817\\signatureverification\\NISDCC-offline-all-001-051-6g\\data.csv')
+    partition = {'train':train_IDs,'validation': valid_IDs}
+    training_generator = DataGenerator(partition['train'], pairs, path, **params)
+    #print(training_generator.labels)
+    validation_generator = DataGenerator(partition['validation'], pairs, path, **params)
+    base_network = create_base_network_signet(input_shape)
+    print(base_network.summary())
+    input_a = Input(shape=input_shape)
+    input_b = Input(shape=input_shape)
+    processed_a = base_network(input_a)
+    processed_b = base_network(input_b)
+    distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([processed_a, processed_b])
+    model = Model(input=[input_a, input_b], output=distance)
+    rms = RMSprop(lr=1e-4, rho=0.9, epsilon=1e-08)
+    adadelta = Adadelta()
+    model.compile(loss=contrastive_loss, optimizer=rms)
+    fname = os.path.join('D:\\nkasturi122817\\signatureverification\\NISDCC-offline-all-001-051-6g', 'weights_NISDCC.hdf5')
+    checkpointer = ModelCheckpoint(filepath=fname, verbose=1, save_best_only=True)
+    # history = model.fit_generator(generator=training_generator,
+    #                     validation_data=validation_generator,
+    #                     use_multiprocessing=True,
+    #                     workers=6,  callbacks=[checkpointer], epochs=5 )
+    # print(history.history)
+    model.load_weights(fname)
+    print('Loading Best Weights')
+
+    tr_pred = model.predict_generator(generator=validation_generator)
+    # print(tr_pred)
+    with open('result.txt', 'w') as f:
+        f.write(str(tr_pred))
+        f.close()
