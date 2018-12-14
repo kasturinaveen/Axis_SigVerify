2d2c10dcfbae09c2fde65181200240f25babd3ef
kasturinaveen <43032059+kasturinaveen@users.noreply.github.com>
2018-12-14 10:18:59 +0530

    Update SignatureVerification.py

--- SignatureVerification.py
+++ SignatureVerification.py
@@ -13,8 +13,8 @@ def build_model():
     processed_b = base_network(input_b)
     distance = Lambda(euclidean_distance, output_shape=eucl_dist_output_shape)([processed_a, processed_b])
     model = Model(input=[input_a, input_b], output=distance)
-    # rms = RMSprop(lr=1e-4, rho=0.9, epsilon=1e-08)
+    rms = RMSprop(lr=1e-4, rho=0.9, epsilon=1e-08)
     # adadelta = Adadelta()
-    # model.compile(loss=contrastive_loss, optimizer=rms, metrics= ['accuracy'])
+    model.compile(loss=contrastive_loss, optimizer=rms, metrics= ['accuracy'])
     model.load_weights("model_prep_acc.h5")
     return model
