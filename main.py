7c4805a4d8578011be95e18fc151eaf897496f75
kasturinaveen <43032059+kasturinaveen@users.noreply.github.com>
2018-12-14 10:18:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, flash, redirect, jsonify
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
 import glob
+import numpy as np
 import sys
 import _pickle as pickle
 import os
@@ -29,15 +30,14 @@ def newcus_upload():
     filename = secure_filename(file.filename)
     cus_info = request.form
     cus_id = cus_info['id']
-
     filename = cus_id + ".png"
-    file.save("D:\\Work\\axis bank\\to_be_named\\data\\" + filename)
+    file.save("D:\\nkasturi122817\\axisbank\\Axis_SigVerify-master\\data\\" + filename)
     return jsonify(filename)
 
 
 @app.route("/retrieve", methods=['GET'])
 def retrieve_id():
-    files = glob.glob("D:\\Work\\axis bank\\to_be_named\\data\\*.*")
+    files = glob.glob("D:\\nkasturi122817\\axisbank\\Axis_SigVerify-master\\data\\*.*")
     for i in range(len(files)):
         files[i] = files[i].split('\\')[-1].split('.')[0]
     return jsonify(files)
@@ -45,23 +45,64 @@ def retrieve_id():
 
 @app.route("/verify", methods=['POST'])
 def verify_customer():
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
-    file = request.files['file']
-    cus_info = request.form
-    cus_id = cus_info['id']
-    filename = cus_id+"_verf.png"
-    file.save("D:\\Work\\axis bank\\to_be_named\\dataVerify\\" + filename)
-    org_img = "D:/Work/axis bank/to_be_named/data/" + cus_id + ".png"
-    prep_org_img = preprocess_image(org_img)
-    img1 = prep_image.img_to_array(prep_org_img)
-    verf_img = "D:/Work/axis bank/to_be_named/dataVerify/" + cus_id+"_verf.png"
-    prep_verf_img = preprocess_image(verf_img)
-    img2 = prep_image.img_to_array(prep_verf_img)
     with graph.as_default():
-        distance = model.predict([[img1], [img2]])
-    result_json = jsonify({"distance": distance.tolist()})
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        cus_info = request.form
+        cus_id = cus_info['id']
+        filename = cus_id+"_verf.png"
+        file.save("D:\\nkasturi122817\\axisbank\\Axis_SigVerify-master\\dataVerify\\" + filename)
+        verf_img = "D:\\nkasturi122817\\axisbank\\Axis_SigVerify-master/dataVerify/" + cus_id+"_verf.png"
+        prep_verf_img = preprocess_image(verf_img)
+        img2 = prep_image.img_to_array(prep_verf_img)
+        org_img_paths = glob.glob("D:\\nkasturi122817\\axisbank\\Axis_SigVerify-master/data/" + cus_id + "*.png")
+        distance = 0
+        num_smpls = len(org_img_paths)
+        input_shape = (155, 220, 1)
+        X1 = np.empty((num_smpls, *input_shape))
+        X2 = np.empty((num_smpls, *input_shape))
+        for i, org_img in zip(range(num_smpls), org_img_paths):
+            prep_org_img = preprocess_image(org_img)
+            img1 = prep_image.img_to_array(prep_org_img)
+            X1[i,] = img1
+            X2[i,] = img2
+        distance  = model.predict([X1, X2])
+        print(distance)
+        avg_distance = (distance.sum()/len(org_img_paths))
+        result_json = jsonify({"distance": avg_distance.tolist()})
+    return result_json
+
+@app.route("/finetune", methods=['POST'])
+def finetune_model():
+    with graph.as_default():
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        cus_info = request.form
+        cus_id = cus_info['id']
+        filename = cus_id+"_verf.png"
+        file.save("D:\\nkasturi122817\\axisbank\\Axis_SigVerify-master\\dataVerify\\" + filename)
+        verf_img = "D:\\nkasturi122817\\axisbank\\Axis_SigVerify-master/dataVerify/" + cus_id+"_verf.png"
+        prep_verf_img = preprocess_image(verf_img)
+        img2 = prep_image.img_to_array(prep_verf_img)
+        org_img_paths = glob.glob("D:\\nkasturi122817\\axisbank\\Axis_SigVerify-master/data/" + cus_id + "*.png")
+        distance = 0
+        num_smpls = len(org_img_paths)
+        input_shape = (155, 220, 1)
+        X1 = np.empty((num_smpls, *input_shape))
+        X2 = np.empty((num_smpls, *input_shape))
+        for i, org_img in zip(range(num_smpls), org_img_paths):
+            prep_org_img = preprocess_image(org_img)
+            img1 = prep_image.img_to_array(prep_org_img)
+            X1[i,] = img1
+            X2[i,] = img2
+        distance  = model.fit([X1, X2],[0])
+        print(distance)
+        avg_distance = (distance.sum()/len(org_img_paths))
+        result_json = jsonify({"distance": avg_distance.tolist()})
     return result_json
 
 
